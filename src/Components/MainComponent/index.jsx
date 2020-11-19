366bd47b8990e8d14b0327988213f13803e090ca
swadesh jaiswal <swadesh.jaiswal.33@gmail.com>
2020-11-19 20:39:55 +0530

    Initial commit

---
+++ src/Components/MainComponent/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sidebar from "../Sidebar";
+import "./mainComponent.css";
+
+const MainComponent = () => {
+  return (
+    <div>
+      <Sidebar classname="sidebar" />
+    </div>
+  );
+};
+export default MainComponent;
---
+++ src/Components/Sidebar/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./sidebar.css";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      Hellooo
+    </div>
+  );
+};
+export default Sidebar;
